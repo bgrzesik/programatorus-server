080aaf8f7a21e4bfe6e1fa06a062707e40efdc98
Bartłomiej Grzesik <bartlomiej.grzesik0@gmail.com>
2022-11-13 23:43:28 +0100

    Remove unused code and refactor PairThread to PairingAgent

--- server/comm/listener/bt.py
+++ server/comm/listener/bt.py
@@ -1,11 +1,145 @@
+import os
+import select
 import logging
 import socket
 import subprocess
+from enum import Enum
+from typing import Optional
+from concurrent.futures import Future
+from abc import ABC, abstractmethod
 
 import bluetooth as bt
 
 from server.actor import Actor
 from server.comm.listener.listener import SocketListener
+
+
+class PairingState(Enum):
+    INACTIVE = 0
+    AGENT_SETUP = 1
+    AWAITING_CONNECTION = 2
+    AWAITING_INPUT = 3
+
+
+class PairingClient(ABC):
+
+    @abstractmethod
+    def on_state_changed(self, state: PairingState):
+        raise NotImplementedError
+
+    @abstractmethod
+    def confirm_pin(self, pin: str) -> Future[bool]:
+        raise NotImplementedError
+
+
+class PairingAgent(Actor):
+
+    def __init__(self, client: PairingClient):
+        super().__init__()
+        self._client = client
+        self._state = PairingState.INACTIVE
+        self._client.on_state_changed(PairingState.INACTIVE)
+        self._proc: Optional[subprocess.Popen] = None
+
+        self._event, self._notify = os.pipe()
+        os.set_blocking(self._event, False)
+
+    @property
+    def state(self) -> PairingState:
+        return self._state
+
+    def _set_state(self, state):
+        self._state = state
+        self._client.on_state_changed(state)
+
+    def cancel(self):
+        logging.debug("cancel():")
+        os.write(self._notify, b"\0")
+
+    def drain_event(self):
+        try:
+            while os.read(self._event, 1):
+                pass
+        except BlockingIOError:
+            pass
+
+    @Actor.handler(guarded=True)
+    def pair(self):
+        try:
+            self._set_state(PairingState.AGENT_SETUP)
+            logging.debug("pair(): Setting up agent")
+
+            subprocess.run(["bluetoothctl", "discoverable", "on"])
+            self.drain_event()
+
+            with subprocess.Popen("bluetoothctl",
+                                  stdout=subprocess.PIPE,
+                                  stdin=subprocess.PIPE) as proc:
+                self._proc = proc
+                self._canceled = False
+
+                assert proc.stdin and proc.stdout
+                lines = [
+                    b"discoverable on\n",
+                    b"agent off\n",
+                    b"agent DisplayYesNo\n",
+                    b"default-agent\n",
+                ]
+                for line in lines:
+                    proc.stdin.write(line)
+
+                self._set_state(PairingState.AWAITING_CONNECTION)
+                logging.debug("pair(): Awaiting connection")
+
+                pin = None
+                buf = []
+                while pin is None:
+                    read, [], [] = select.select([self._event, proc.stdout],
+                                                 [], [])
+
+                    if self._event in read:
+                        self.drain_event()
+                        return
+                    elif proc.stdout not in read:
+                        continue
+
+                    buf.append(chr(proc.stdout.read(1)[0]))
+                    if len(buf) > len("passkey 000000 (yes/no):"):
+                        buf.pop(0)
+
+                    if "".join(buf).endswith("(yes/no):"):
+                        pin = "".join(buf[len("passkey "):-len(" (yes/no):")])
+                        break
+
+                    if self._canceled:
+                        return
+
+                self._set_state(PairingState.AWAITING_INPUT)
+                logging.info(f"pair(): pin={pin}")
+
+                if pin and self._client.confirm_pin(pin).result():
+                    proc.stdin.write(b"yes\ntrust\n")
+                else:
+                    proc.stdin.write(b"no\n")
+
+                self._set_state(PairingState.INACTIVE)
+
+        except Exception as exc:
+            logging.error("", exc_info=exc)
+            raise
+        finally:
+            self._proc = proc
+            self._canceled = False
+            try:
+                subprocess.run(["bluetoothctl", "pairable", "off"])
+                subprocess.run(["bluetoothctl", "discoverable", "off"])
+                logging.debug("pair(): Disabled discoverability")
+            except Exception as exc:
+                logging.error("", exc_info=exc)
+                raise
+            finally:
+                if self._state != PairingState.INACTIVE:
+                    self._set_state(PairingState.INACTIVE)
 
 
 class BluetoothListener(SocketListener, Actor):
--- server/ui/pair.py
+++ server/ui/pair.py
@@ -1,40 +1,50 @@
-from server.comm.transport.bt_deprecated import (
-    PairThread,
-    INACTIVE,
-    AGENT_SETUP,
-    AWAITING_CONNECTION,
-)
+from concurrent.futures import Future
 
+from server.comm.listener.bt import PairingAgent, PairingClient, PairingState
 from server.ui.menu import MenuItem, FONT
 
 
-class PairDialog(MenuItem):
+class PairDialog(MenuItem, PairingClient):
     def __init__(self):
         super().__init__("Enable pairing")
         self.height = 16
         self.width = 96
-        self.pairThread = PairThread()
+        self._agent = PairingAgent(self)
+        self._pin = "000000"
+        self._future: Future[bool] = Future()
+
+    def on_state_changed(self, state):
+        if state == PairingState.INACTIVE:
+            self.text = "Enabling pairing"
+        elif state == PairingState.AGENT_SETUP:
+            self.text = "Please wait..."
+        elif state == PairingState.AWAITING_CONNECTION:
+            self.text = "Connect now"
+        elif state == PairingState.AWAITING_INPUT:
+            self.text = f"Pin: {self._pin}"
+
+    def confirm_pin(self, pin) -> Future[bool]:
+        self._future = Future()
+        self._pin = pin
+        self.on_state_changed(PairingState.AWAITING_INPUT)
+        return self._future
 
     @property
     def visible_text(self):
-        return self.pairThread.display_text + "\n" + "YES [OK]    NO [<-]"
+        return self.text + "\n" + "YES [OK]    NO [<-]"
 
     def on_click(self, select=True):
-        print("clicked", select)
-        if self.pairThread.state == INACTIVE:
-            self.pairThread = PairThread()
-            self.pairThread.start()
-        elif self.pairThread.state == AGENT_SETUP:
-            pass
-        elif self.pairThread.state == AWAITING_CONNECTION:
-            pass
-        else:
-            self.pairThread.notify(select)
+        if self._agent.state == PairingState.INACTIVE:
+            self._agent.pair()
+        elif self._agent.state == PairingState.AWAITING_CONNECTION:
+            self._agent.cancel()
+        elif self._agent.state == PairingState.AWAITING_INPUT:
+            self._future.set_result(select)
 
     def draw(self, draw, x, y):
         if self.is_selected:
             draw.rectangle((x, y, x + self.width, y +
                            self.height), outline=1, fill=0)
 
-        draw.text((x + 1, y), self.pairThread.display_text, font=FONT, fill=1)
+        draw.text((x + 1, y), self.text, font=FONT, fill=1)
         draw.text((x + 1, y + 8), "YES [OK]    NO [<-]", font=FONT, fill=1)
